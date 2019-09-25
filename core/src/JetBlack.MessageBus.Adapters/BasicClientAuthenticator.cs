0112b7155ef0a5a5acaa81b2ce9d4f470228c6fa
Rob Blackbourn <rblackbourn@ugsb-rbla01.bhdgsystematic.com>
2019-09-25 10:00:20 +0100

    Added application to the client connection string

--- core/src/JetBlack.MessageBus.Adapters/BasicClientAuthenticator.cs
+++ core/src/JetBlack.MessageBus.Adapters/BasicClientAuthenticator.cs
@@ -7,17 +7,24 @@ namespace JetBlack.MessageBus.Adapters
     public class BasicClientAuthenticator : ClientAuthenticator
     {
         private readonly string _password;
-        public BasicClientAuthenticator(string username, string password, string? impersonating = null, string? forwardedFor = null)
+        public BasicClientAuthenticator(
+            string username,
+            string password,
+            string? impersonating = null,
+            string? forwardedFor = null,
+            string? application = null)
         {
             Username = username;
             _password = password;
             Impersonating = impersonating;
             ForwardedFor = forwardedFor;
+            Application = application;
         }
 
         public string Username { get; }
         public string? Impersonating { get; }
         public string? ForwardedFor { get; }
+        public string? Application { get; }
 
         protected override string ToConnectionString()
         {
@@ -28,12 +35,15 @@ namespace JetBlack.MessageBus.Adapters
                 connectionString.Append(';').Append("Impersonating=").Append(Impersonating);
             if (!string.IsNullOrWhiteSpace(ForwardedFor))
                 connectionString.Append(';').Append("ForwardedFor=").Append(ForwardedFor);
+            if (!string.IsNullOrWhiteSpace(Application))
+                connectionString.Append(';').Append("Application=").Append(Application);
             return connectionString.ToString();
         }
 
         public override string ToString() =>
             $"{nameof(Username)}=\"{Username}\"" +
             $",{nameof(Impersonating)}=\"{Impersonating}\"" +
-            $",{nameof(ForwardedFor)}=\"{ForwardedFor}\"";
+            $",{nameof(ForwardedFor)}=\"{ForwardedFor}\"" +
+            $",{nameof(Application)}=\"{Application}\"";
     }
 }
--- core/src/JetBlack.MessageBus.Adapters/TokenClientAuthenticator.cs
+++ core/src/JetBlack.MessageBus.Adapters/TokenClientAuthenticator.cs
@@ -6,16 +6,22 @@ namespace JetBlack.MessageBus.Adapters
 {
     public class TokenClientAuthenticator : ClientAuthenticator
     {
-        public TokenClientAuthenticator(string token, string? impersonating = null, string? forwardedFor = null)
+        public TokenClientAuthenticator(
+            string token,
+            string? impersonating = null,
+            string? forwardedFor = null,
+            string? application = null)
         {
             Token = token;
             Impersonating = impersonating;
             ForwardedFor = forwardedFor;
+            Application = application;
         }
 
         public string Token { get; }
         public string? Impersonating { get; }
         public string? ForwardedFor { get; }
+        public string? Application { get; }
 
         protected override string ToConnectionString()
         {
@@ -25,12 +31,15 @@ namespace JetBlack.MessageBus.Adapters
                 connectionString.Append(';').Append("Impersonating=").Append(Impersonating);
             if (!string.IsNullOrWhiteSpace(ForwardedFor))
                 connectionString.Append(';').Append("ForwardedFor=").Append(ForwardedFor);
+            if (!string.IsNullOrWhiteSpace(Application))
+                connectionString.Append(';').Append("Application=").Append(Application);
             return connectionString.ToString();
         }
 
         public override string ToString() =>
             $"{nameof(Token)}=\"{Token}\"" +
             $",{nameof(Impersonating)}=\"{Impersonating}\"" +
-            $",{nameof(ForwardedFor)}=\"{ForwardedFor}\"";
+            $",{nameof(ForwardedFor)}=\"{ForwardedFor}\"" +
+            $",{nameof(Application)}=\"{Application}\"";
     }
 }
