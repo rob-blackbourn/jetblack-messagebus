9a975c0fbadf1a426a6c34c177eaaac63735cf90
Rob Blackbourn <rob.blackbourn@gmail.com>
2021-08-04 14:08:45 +0100

    Added a test for interactor roles

---
+++ core/test/JetBlack.MessageBus.Distributor.Test/Roles/InteractorRoleTest.cs
@@ -0,0 +1,22 @@
+#nullable enable
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JetBlack.MessageBus.Distributor.Roles;
+
+namespace JetBlack.MessageBus.Distributor.Test.Roles
+{
+    [TestClass]
+    public class InteractorRoleTest
+    {
+        [TestMethod]
+        public void ShouldUnderstandRoles()
+        {
+            var role = new InteractorRole("host", "user", Role.Subscribe, Role.Publish | Role.Notify | Role.Authorize);
+            Assert.IsTrue(role.HasRole(Role.Subscribe, true));
+            Assert.IsFalse(role.HasRole(Role.Publish, true));
+            Assert.IsFalse(role.HasRole(Role.Notify, true));
+            Assert.IsFalse(role.HasRole(Role.Authorize, true));
+        }
+    }
+}
